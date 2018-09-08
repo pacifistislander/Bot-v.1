c0e95aa9f8878ea09e0608f7b7c5fa02b2c5248c
LeotomasMC <marycolageo@gmail.com>
2018-09-08 12:52:52 -0400

    THATS ALOTTA CHANGES
    added a few things, and the presence timer should work

--- Bot v1.py
+++ Bot v1.py
@@ -1,32 +1,59 @@
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
 import asyncio
 import time
+import random
 
+Client = discord.Client() 
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = "Sp/")   #certain commands that require a bot prefix must start with the prefix in the ""
+if True: #this lets you fold the code is supported editors to take less room
+    presences = [ # add any texts you want here. This is valid syntax, as long as you end the list with the ] properly
+        "with the Real Spyro",
+        "with your emotions",
+        "catch with a grenade",
+        "contemplating suicide"
+    ]
+    
+    presenceChangeTime = 20 # increased from 10 because 10 seconds is too short
 
 async def status_task():
+    global presences
+    global presenceChangeTime
     while True:
-        await client.change_presence("with the Real spyro")
-        await asyncio.sleep(10)
-        await client.change_presence("with your emotions")
-        await asyncio.sleep(10)
-        await client.change_presence("catch with a grenade")
-        await asyncio.sleep(10)
-        await client.change_presence("contemplating suicide")
-        await asyncio.sleep(10)
-
+        newPresence = discord.Game(name=random.choice(presences))
+        await client.change_presence(newPresence)
+        await asyncio.sleep(presenceChangeTime)
+        
+async def chat(channel, message=None, tts=False, embed=None):
+    if message == None and embed == None:
+        return False
+    else:
+        try:
+            msg = await client.send_message(channel, message=message, tts=tts, embed=embed)
+            return msg
+        except discord.errors.Forbidden:
+            ep('The bot is not allowed to send messages to this channel')
+            return None
+        
 @client.event
 async def on_ready():
-    print("SpyroBot status: ONLINE")   #if the bot is online, it sends this message
-    client.loop.create_task(status_task())
-    
+    print("SpyroBot status: ONLINE")
     
 @client.event
 async def on_message(message):
+    author = message.author
+    ch = message.channel
+    server = message.server
+    
+    arguments = message.content.split(' ')
+    command = arguments[0].lower()
+    
+    while len(arguments) < 15:
+        arguments.append('')
+    else:
+        pass # wow
+    
+    # TODO: rewrite the rest of the on_message function to use the new chat function, and use the variables created above
+    
     if message.content == "Who is the best streamer?":  #this checks to see if someone has sent a message with the same text that is in the ""
         await client.send_message(message.channel, "Obviously its SpyroL7")  #if the above statement = true, the bot sends the message in the ""
                                 #message.channel means the text is posted in the same channel as the trigger message was sent in
@@ -51,5 +78,5 @@ async def on_message(message):
 
     
 
-
-client.run("NDgzNTkxNjIyNDI1MDUxMTM2.Dmw9Mg._smm7HVKuuJXNG0M4KUl4KmsTjs")   #the bots token - used to connect the script and the app
+client.loop.create_task(status_task())
+client.run("<TOKEN>")   #the bots token - used to connect the script and the app
