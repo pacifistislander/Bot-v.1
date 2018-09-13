2a966ce24c2fabf163b995007f462d817670bc40
Spyro <43008046+SpyroL7@users.noreply.github.com>
2018-09-13 18:15:04 +0100

    new version
    more commands and that

--- Bot v1.py
+++ Bot v1.py
@@ -5,25 +5,14 @@ import random
 
 client = discord.Client() 
 
-if True: #this lets you fold the code is supported editors to take less room
-    presences = [ # add any texts you want here. This is valid syntax, as long as you end the list with the ] properly
-        "with the Real Spyro",
-        "with your emotions",
-        "catch with a grenade",
-        "contemplating suicide"
-    ]
-    
-    presenceChangeTime = 20 # increased from 10 because 10 seconds is too short
-
 async def status_task():
-    global client
-    global presences
-    global presenceChangeTime
     while True:
-        newPresence = discord.Game(name=random.choice(presences))
-        await client.change_presence(game=newPresence)
-        await asyncio.sleep(presenceChangeTime)
-        
+        await client.change_presence("with the real Spyro")
+        await asyncio.sleep(20)
+        await client.change_presence("catch with a hand grenade")
+        await asyncio.sleep(20)
+
+      
 async def chat(channel, message=None, tts=False, embed=None):
     if message == None and embed == None:
         return False
@@ -38,6 +27,7 @@ async def chat(channel, message=None, tts=False, embed=None):
 @client.event
 async def on_ready():
     print("SpyroBot status: ONLINE")
+    client.loop.create_task(status_task())
     
 @client.event
 async def on_message(message):
@@ -60,9 +50,31 @@ async def on_message(message):
                                 #message.channel means the text is posted in the same channel as the trigger message was sent in
     if message.content == "Who is the worst streamer?":  
         await client.send_message(message.channel, "Matt65 lol")
-    if message.content == "Alexa play despacito":  
+    if message.content == "Alexa play despacito".lower():  
         await client.send_message(message.channel, " ```Playing Despacito by Louis Fonsi ft. Daddy Yankee``` ")
     if message.content == "Press F to pay respects":  
+        await client.send_message(message.channel, "F")
+    if message.content == "Who is the best bot":  
+        await client.send_message(message.channel, "Well thats me of course! Want to add me to your server? Here's a link: https://discordapp.com/api/oauth2/authorize?client_id=483591622425051136&permissions=8&scope=bot ")
+    if message.content == "Good bot":  
+        await client.send_message(message.channel, "*excited borking*")
+    if message.content == "Bad bot":  
+        await client.send_message(message.channel, "Well fuck you too")
+    if message.content == "@Spyroboto#4192 ":  
+        await client.send_message(message.channel, "omg what. Like seriously did you have to ping me? I was taking a cyber nap and you woke me. pings are SO ANNOYING - like especially going live ones, but at least you can supress @ everyones. Did I tell you about the time I got banned from a server with like 30k people? Its true! I was kicked a fair bit before permabanned. Spyro didn't have admin privs, but i did so he just told me to kick people he didn't like lol. I woulda gotten away with it if it weren't for those meddling ~~kids~~ audit logs. Sorry for rambling a bit hope you enjoyed smash like, subscribe, follow and donate all your money. All of it. Right now. Pls :3 ")
+    if message.content == ";-;":  
+        await client.send_message(message.channel, "this")
+        time.sleep(0.3)
+        await client.send_message(message.channel, "is")
+        time.sleep(0.3)
+        await client.send_message(message.channel, "so")
+        time.sleep(0.3)
+        await client.send_message(message.channel, "sad")
+    if message.content == "cool story bro":  
+        await client.send_message(message.channel, "tell it again")
+    if message.content == "Indeed":  
+        await client.send_message(message.channel, "Good meme")
+    if message.content == "":  
         await client.send_message(message.channel, "F")
     if message.content == "This is so sad":  
         await client.send_message(message.channel, ";-;")
@@ -72,12 +84,13 @@ async def on_message(message):
         await client.send_message(message.channel, " ```Ew``` ")
     if message.content == "Hello":  
         await client.send_message(message.channel, "Go away no one likes you")
+    if message.content == "yeet":  
+        await client.send_message(message.channel, "ok hold up. Did you seriously just say yeet? like what are you, 10 years old? Have you only 3 working brain cells?! Like there must be something genuinely wrond with your brain. When was the last time you had it scanned. Really brings back memories of LONG DEAD MEMES. I mean, 2016 may have been a better time but like do you remember the quilaity of memes? Like that gorilla - all it did was DIE and it became a meme?! Like if that was how it is now i would be liking more of those 'like to instantly die' memes ")
     if message.content.startswith('Shrek'):
         await client.kick(message.author)
-
-
-
     
 
-client.loop.create_task(status_task())
-client.run("<TOKEN>")   #the bots token - used to connect the script and the app
+
+
+
+client.run("TOKEN")   #the bots token - used to connect the script and the app
